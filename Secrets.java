291fe40d57f9c1c39320f6563327d6590321d21c
Joran <52299710+TacticalDevv@users.noreply.github.com>
2019-06-28 10:56:35 +0200

    Delete Secrets.java

--- Secrets.java
+++
@@ -1,8 +0,0 @@
-package me.vortexgames.vtgbot;
-
-public class Secrets {
-
-    public static String TOKEN = "NTkzMDc2Mjk3NzE1MTU0OTU0.XRIngQ.jSp7W7fbBUhcC69QaS6-G2HAM0U";
-    public static String BOTLINK = "https://discordapp.com/oauth2/authorize?client_id=593076297715154954&scope=bot&permissions=2146958847";
-
-}
